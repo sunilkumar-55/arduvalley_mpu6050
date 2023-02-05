e1588b46301812baaaf2f7e801fce6e6b86f0b80
Sunil kumar <88759435+sunilkumar105@users.noreply.github.com>
2023-02-05 09:10:34 +0530

    Update arduvalley_mpu6050.h

--- arduvalley_mpu6050.h
+++ arduvalley_mpu6050.h
@@ -6,15 +6,16 @@
 // here we put the prototype of the functions
 void initialise_gyro();
 
-int16_t read_accel_x();
-int16_t read_accel_y();
-int16_t read_accel_z();
+int16_t read_accel_x_raw();
+int16_t read_accel_y_raw();
+int16_t read_accel_z_raw();
 
-int16_t read_gyro_x();
-int16_t read_gyro_y();
-int16_t read_gyro_z();
+int16_t read_gyro_x_raw();
+int16_t read_gyro_y_raw();
+int16_t read_gyro_z_raw();
 
 float read_temp_in_cel();
-
+float read_orientation_x();
+float read_orientation_y();
 
 #endif
